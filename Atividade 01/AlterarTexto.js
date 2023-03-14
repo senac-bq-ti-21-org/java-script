af85557a45561ca6ad767f132163c16edc33c2b5
KaioHSG <kaio12hsg@gmail.com>
2023-03-14 08:51:12 -0300

    Update

--- Atividade 01/AlterarTexto.js
+++ Atividade 01/AlterarTexto.js
@@ -1,2 +1,7 @@
-var titulo = document.querySelector("#titulo");
-titulo.textContent = "Texto alterado";
+var titulo = document.querySelectorAll(".titulo");
+titulo[0].textContent = "Texto alterado";
+titulo[1].textContent = "Texto alterado 2";
+function mostrarAlerta() {
+    alert("Mensagem");
+}
+titulo[0].onclick = mostrarAlerta;
