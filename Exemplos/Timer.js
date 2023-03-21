b03e455226ac34cf58001643fc7acaee4cab103f
KaioHSG <kaio12hsg@gmail.com>
2023-03-21 08:27:41 -0300

    Update

---
+++ Exemplos/Timer.js
@@ -0,0 +1,6 @@
+//Executa a função fundo em um intervalo de 10s.
+setTimeout(fundo, 10000);
+//Executa a função fundo em um intervalo de 4s.
+setTimeout(fundo, 4000);
+var timer = setInterval(fundo, 1000);
+clearInterval(tempo);
